6489f4ed47a5abc4facecc6f4b844a7bec59b190
Lucas Fischer <lucas.fischer@me.com>
2026-02-17 21:14:38 +0800

    fix URL.amoreServer

--- Sources/AmoreLicensing/Extensions/URL+AmoreServer.swift
+++ Sources/AmoreLicensing/Extensions/URL+AmoreServer.swift
@@ -2,5 +2,5 @@ import Foundation
 
 public extension URL {
     /// The base URL for the Amore licensing server.
-    static let amoreServer: URL = URL(string: "http://127.0.0.1:8080")!
+    static let amoreServer: URL = URL(string: "https://api.amore.computer")!
 }
